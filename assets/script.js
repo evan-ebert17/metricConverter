6d816b493b6ac00296c4a17b42820df4f4eab648
Dave Strider <evan.ebert17@gmail.com>
2022-06-06 01:24:52 -0500

    turns out you cannot nest other HTML elements inside of option tags, so... they are gone

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,18 @@
+var selectOptFrom = document.getElementById("compareFrom");
+var selectOptTo = document.getElementById("compareTo");
+var selectedUni = document.querySelector(".selectedUnit");
+var units = document.getElementById("unitSelect");
+// var optVal = selectOptFrom.value;
+// var metVal = 10**selectOptFrom.value
+var convBtn = document.getElementById("convertBtn");
+// selectedUni.textContent = units.value;
 
+
+convBtn.addEventListener('click', function() {
+    // alert(selectOptFrom.value);
+    // alert(10**selectOptFrom.value)
+    alert(selectedUni.innerHTML)
+})
 
 
 
