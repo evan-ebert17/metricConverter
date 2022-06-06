71c99478a7b71db287f2e542e838f6c41a99bedf
Dave Strider <evan.ebert17@gmail.com>
2022-06-06 18:57:59 -0500

    updated code to produce the conversions into the right hand box, need to fix later though

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,8 @@
 var selectOptFrom = document.getElementById("compareFrom");
 var selectOptTo = document.getElementById("compareTo");
 var selectedUni = document.querySelector(".selectedUnit");
+var textFrom = document.getElementById("convertFrom")
+var textTo = document.getElementById("convertTo")
 var units = document.getElementById("unitSelect");
 // var optVal = selectOptFrom.value;
 // var metVal = 10**selectOptFrom.value
@@ -8,10 +10,22 @@ var convBtn = document.getElementById("convertBtn");
 // selectedUni.textContent = units.value;
 
 
-convBtn.addEventListener('click', function() {
-    // alert(selectOptFrom.value);
-    // alert(10**selectOptFrom.value)
-    alert(selectedUni.innerHTML)
+convBtn.addEventListener('click', function () {
+    //take value, make that the exponent
+    //when clicked, turns the other thing into the exponent of that form.
+    if (textFrom.value == "" || isNaN(textFrom.value)) {
+        alert("Please type a number into the left-hand box before converting.")
+    } else {
+        //subtract value from value to get amount to move over
+        if(selectOptFrom.value < 0) {
+            var exponentGiveNeg = selectOptFrom + selectOptTo;
+            textTo.value = 10**exponentGiveNeg;
+        } else if(selectOptFrom.value && selectOptTo.value > 0){
+            var exponentGivePos = selectOptFrom - selectOptTo;
+            textTo.value = 10**exponentGivePos;
+        }
+        console.log(textTo.value);
+    }
 })
 
 
